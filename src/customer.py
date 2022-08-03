0502278d3be6122ef078f581d4258ebf7bdbb948
Benjamin Cutler-Ames <bencutames@gmail.com>
2022-08-03 11:47:26 +0100

    MVP complete

--- src/customer.py
+++ src/customer.py
@@ -1,2 +1,14 @@
+from src.pub import *
+
 class Customer:
     pass
+
+    def __init__(self,name,wallet,age):
+        self.name = name
+        self.wallet = wallet
+        self.age = age
+
+    def buy_drink(self,drink,pub):
+        self.wallet -= drink.price
+        pub.increase_till(drink.price)
+
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,5 +1,16 @@
 import unittest
 from src.customer import Customer
+from src.pub import Pub
+from src.drink import Drink
+
 
 class TestCustomer(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.customer = Customer("Dave", 50,37)
+
+    def test_buy_drink(self):
+        pub = Pub("The Prancing Pony", 100)
+        drink = Drink("Gin and tonic", 5)
+        self.customer.buy_drink(drink, pub)
+        self.assertEqual(45, self.customer.wallet)
+        self.assertEqual(105, pub.till)
